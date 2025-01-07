adc00d1bf858b418543fabe5b544b902c4fac827
LeonBijker <0351849@student.rocvantwente.nl>
2025-01-07 10:27:13 +0100

    feat: added movement features
    added sprinting and tried to add wallrunning ( not done)

--- Assets/Scripts/movement/Player_movement.cs
+++ Assets/Scripts/movement/Player_movement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -7,17 +8,26 @@ public class Movement : MonoBehaviour
     float horizontalInput;
     float verticalInput;
     bool isGrounded;
-    float speed = 5f;
+    bool isWallRunning;
+    bool isNearWall;
+    float baseSpeed = 5f;
+    float sprintMultiplier = 2f;
+    float wallRunSpeed = 7f;
+    float speed;
     float jumpForce = 7.5f;
     float airMultiplier = 0.5f;
     int jumpCount = 0;
     int maxJumps = 2;
     Rigidbody body;
     Vector3 moveDirection;
-    string layername = "ground";
+    Vector3 wallJumpDirection;
+    string groundLayer = "ground";
+    string wallLayer = "wall";
 
-    [SerializeField]float groundDrag = 4f;
-    [SerializeField]float airDrag = 1f;
+    [SerializeField] float groundDrag = 4f;
+    [SerializeField] float airDrag = 1f;
+    [SerializeField] float wallRunGravity = 2f;
+    [SerializeField] float wallDistance = 1.5f;
 
     void Start()
     {
@@ -25,6 +35,8 @@ public class Movement : MonoBehaviour
         body.freezeRotation = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        speed = baseSpeed;
     }
 
     void Update()
@@ -32,19 +44,32 @@ public class Movement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        CheckWallRunning();
+
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            speed = baseSpeed * sprintMultiplier;
+        }
+        else
+        {
+            speed = baseSpeed;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
             {
-                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                isGrounded = false;
-                jumpCount = 1;
+                Jump();
             }
             else if (jumpCount < maxJumps)
             {
-                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                jumpCount++;
+                Jump();
             }
+        }
+
+        if (isWallRunning && Input.GetKeyDown(KeyCode.Space))
+        {
+            WallRunJump();
         }
 
         SpeedControl();
@@ -53,7 +78,14 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        MovePlayer();
+        if (isWallRunning)
+        {
+            WallRunMovement();
+        }
+        else
+        {
+            MovePlayer();
+        }
     }
 
     private void MovePlayer()
@@ -68,6 +100,39 @@ public class Movement : MonoBehaviour
         {
             body.AddForce(moveDirection.normalized * speed * 10f * airMultiplier, ForceMode.Force);
         }
+    }
+
+    private void WallRunMovement()
+    {
+        moveDirection = orientation.forward;
+
+        Vector3 wallRunForce = moveDirection.normalized * wallRunSpeed + Vector3.down * wallRunGravity;
+
+        body.AddForce(wallRunForce, ForceMode.Force);
+
+        body.linearVelocity = new Vector3(body.linearVelocity.x, Mathf.Clamp(body.linearVelocity.y, -2f, 2f), body.linearVelocity.z);
+    }
+
+    private void WallRunJump()
+    {
+        body.useGravity = true;
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            wallJumpDirection = transform.up + (-orientation.right);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            wallJumpDirection = transform.up + orientation.right;
+        }
+        body.AddForce(wallJumpDirection.normalized * jumpForce, ForceMode.Impulse);
+        isWallRunning = false;
+    }
+
+    private void Jump()
+    {
+        body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        jumpCount++;
     }
 
     private void SpeedControl()
@@ -95,22 +160,69 @@ public class Movement : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        int layerIndex = LayerMask.NameToLayer(layername);
+        int groundLayerIndex = LayerMask.NameToLayer(groundLayer);
+        int wallLayerIndex = LayerMask.NameToLayer(wallLayer);
 
-        if (collision.gameObject.layer == layerIndex)
+        if (collision.gameObject.layer == groundLayerIndex)
         {
             isGrounded = true;
             jumpCount = 0;
+            isWallRunning = false;
+            body.useGravity = true;
+        }
+        else if (collision.gameObject.layer == wallLayerIndex)
+        {
+            StartWallRun();
         }
     }
 
     void OnCollisionExit(Collision collision)
     {
-        int layerIndex = LayerMask.NameToLayer(layername);
+        int groundLayerIndex = LayerMask.NameToLayer(groundLayer);
+        int wallLayerIndex = LayerMask.NameToLayer(wallLayer);
 
-        if (collision.gameObject.layer == layerIndex)
+        if (collision.gameObject.layer == groundLayerIndex)
         {
             isGrounded = false;
+        }
+        else if (collision.gameObject.layer == wallLayerIndex)
+        {
+            StopWallRun();
+        }
+    }
+
+    private void StartWallRun()
+    {
+        if (!isGrounded && isNearWall)
+        {
+            isWallRunning = true;
+            body.useGravity = false;
+        }
+    }
+
+    private void StopWallRun()
+    {
+        isWallRunning = false;
+        body.useGravity = true;
+    }
+
+    private void CheckWallRunning()
+    {
+        RaycastHit hit;
+        isNearWall = false;
+
+        if (Physics.Raycast(transform.position, orientation.right, out hit, wallDistance) || Physics.Raycast(transform.position, -orientation.right, out hit, wallDistance))
+        {
+            isNearWall = true;
+        }
+        else
+        {
+            isNearWall = false;
+        }
+
+        if (!isNearWall)
+        {
+            StopWallRun();
         }
     }
 }
