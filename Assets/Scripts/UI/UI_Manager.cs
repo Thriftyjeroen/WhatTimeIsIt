9d112f171f19711a6761869b0d2590f05d9cc873
LeonBijker <0351849@student.rocvantwente.nl>
2025-01-16 13:45:03 +0100

    feat: changed UI manager

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
-    public Pausescreen Pausescreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,16 +13,19 @@ public class UI_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+       
     }
-    public void Button(int pScene) => SceneManager.LoadScene(pScene);
-
+    public void Button(string pScene)
+    {
+        if (SceneManager.GetActiveScene().name != pScene) // Avoid reloading the current scene
+        {
+            SceneManager.LoadScene(pScene);
+        }
+        
+    }
     public void Quit()
     {
         Application.Quit();
     }
-    public void Continue()
-    {
-        Pausescreen.PauseScreenOff();
-    }
+    
 }
