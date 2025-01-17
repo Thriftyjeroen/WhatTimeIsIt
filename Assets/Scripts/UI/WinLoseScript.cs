b6c846c30b56c546adc7286f13cd62dc868dd74b
LeonBijker <0351849@student.rocvantwente.nl>
2025-01-17 12:44:24 +0100

    feat: trying to fix shit

---
+++ Assets/Scripts/UI/WinLoseScript.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class WinLoseScript : MonoBehaviour
+{
+    [SerializeField] TMP_Text WinLoseText;
+    [SerializeField] GameObject PopUp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PopUp.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Win() 
+    {
+        PopUp.SetActive(true);
+        WinLoseText.text = "you have won";
+    }
+    public void Lose()
+    {
+        PopUp.SetActive(true);
+        WinLoseText.text = "you have lost";
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.ToLower().Contains("YouWin"))
+        {
+            Win();
+        }
+        if (other.gameObject.name.ToLower().Contains("water.001"))
+        {
+            Lose();
+        }
+    }
+}
