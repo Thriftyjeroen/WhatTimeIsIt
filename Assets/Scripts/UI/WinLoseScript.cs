532c6d3522e7ebd55ef890823e6870c236f9f89c
Lotte Koster <145015221+LotteKoster05@users.noreply.github.com>
2025-01-21 10:59:13 +0100

    Feat: fixed go trough map

--- Assets/Scripts/UI/WinLoseScript.cs
+++ Assets/Scripts/UI/WinLoseScript.cs
@@ -1,33 +1,33 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinLoseScript : MonoBehaviour
 {
-    [SerializeField] TMP_Text WinLoseText,scoretext;
+    [SerializeField] TMP_Text WinLoseText, scoretext;
     [SerializeField] GameObject PopUp;
     [SerializeField] ScoreUploadManager ScoreUploadManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // PopUp.SetActive(false);
+        // PopUp.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void Win() 
+    public void Win()
     {
-        PopUp.SetActive(true);
-        scoretext.text = ScoreUploadManager.fullScore.text;
-        WinLoseText.text = "you have won";
+        StartCoroutine(HandleWin());
     }
     public void Lose()
     {
         PopUp.SetActive(true);
         WinLoseText.text = "you have lost";
+        Time.timeScale = 0f;
 
     }
     private void OnTriggerEnter(Collider other)
@@ -38,12 +38,12 @@ public class WinLoseScript : MonoBehaviour
             Win();
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            Time.timeScale = 0f;
+
         }
         if (other.gameObject.name.ToLower().Contains("water"))
         {
             Lose();
-            Time.timeScale = 0f; 
+
 
         }
     }
@@ -51,4 +51,33 @@ public class WinLoseScript : MonoBehaviour
     {
         SceneManager.LoadScene("LevelSelect");
     }
+
+
+
+    IEnumerator HandleWin()
+    {
+        // Start uploading the score
+        ScoreUploadManager.UploadScore();
+
+        // Wait for the upload to complete
+        while (!ScoreUploadManager.done && !ScoreUploadManager.failed)
+        {
+            yield return null; // Wait for the next frame
+        }
+
+        // Now show the popup
+        PopUp.SetActive(true);
+        scoretext.text = ScoreUploadManager.fullScore.text;
+
+        if (ScoreUploadManager.failed)
+        {
+            WinLoseText.text = "You have won, but the score upload failed.";
+        }
+        else
+        {
+            WinLoseText.text = "You have won, and your score was uploaded.";
+        }
+    }
 }
+
+
