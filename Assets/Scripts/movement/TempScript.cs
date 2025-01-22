70d03f4d56f3f3f25f4f7b090d0e4607f86b1921
Daniël Kosters <0356076@student.rocvantwente.nl>
2025-01-22 10:15:37 +0100

    fix: terein benaming
    het terein heeft een update gekregen.
    de onderdelen hebben namen gekregen waardoor andere scriptjes die kunnen herkennen

--- Assets/Scripts/movement/TempScript.cs
+++ Assets/Scripts/movement/TempScript.cs
@@ -13,7 +13,7 @@ public class TempScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Player.transform.position.y <= -30)
+        if(Player.transform.position.y <= -60)
         {
             WinLoseScript.Lose();
         }
